728dbc46be1ba9c59e709ad974a6d1ad0cf5a9b2
Trio556 <alemat04@gmail.com>
2017-07-29 01:13:22 -0400

    Added ability to remove blocks. Raycasting from modify which is attached to the camera when space bar is pressed

--- Assets/World.cs
+++ Assets/World.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class World : MonoBehaviour {
+public class World : MonoBehaviour
+{
 
     public Dictionary<WorldPos, Chunk> chunks = new Dictionary<WorldPos, Chunk>();
     public GameObject chunkPrefab;
@@ -102,6 +103,23 @@ public class World : MonoBehaviour {
         {
             chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, block);
             chunk.update = true;
+            
+            UpdateIfEqual(x - chunk.pos.x, 0, new WorldPos(x - 1, y, z));
+            UpdateIfEqual(x - chunk.pos.x, Chunk.chunkSize - 1, new WorldPos(x + 1, y, z));
+            UpdateIfEqual(y - chunk.pos.y, 0, new WorldPos(x, y - 1, z));
+            UpdateIfEqual(y - chunk.pos.y, Chunk.chunkSize - 1, new WorldPos(x, y + 1, z));
+            UpdateIfEqual(z - chunk.pos.z, 0, new WorldPos(x, y, z - 1));
+            UpdateIfEqual(z - chunk.pos.z, Chunk.chunkSize - 1, new WorldPos(x, y, z + 1));
+        }
+    }
+
+    void UpdateIfEqual(int value1, int value2, WorldPos pos)
+    {
+        if (value1 == value2)
+        {
+            Chunk chunk = GetChunk(pos.x, pos.y, pos.z);
+            if (chunk != null)
+                chunk.update = true;
         }
     }
 }
