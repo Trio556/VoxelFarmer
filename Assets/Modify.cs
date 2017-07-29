5d44125cdf4cf83cc3ea80ddb7feb61a1015b987
Trio556 <alemat04@gmail.com>
2017-07-29 16:11:11 -0400

    Added modify to character so every left mouse click removes a cube. Some old code was removed that was transforming the camera that was not needed anymore. Eventually this maybe removed from the first person character and placed on items

--- Assets/Modify.cs
+++ Assets/Modify.cs
@@ -5,7 +5,6 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Modify : MonoBehaviour
 {
-    Vector2 rot;
 
 	// Use this for initialization
 	void Start ()
@@ -16,23 +15,13 @@ public class Modify : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        //if (CrossPlatformInputManager.GetButtonDown(""))
-        //{
-        //    RaycastHit hit;
-        //    if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
-        //    {
-        //        EditTerrain.SetBlock(hit, new BlockAir());
-        //    }
-        //}
-
-        rot = new Vector2(
-            rot.x + Input.GetAxis("Mouse X") * 3,
-            rot.y + Input.GetAxis("Mouse Y") * 3);
-
-        transform.localRotation = Quaternion.AngleAxis(rot.x, Vector3.up);
-        transform.localRotation *= Quaternion.AngleAxis(rot.y, Vector3.left);
-
-        transform.position += transform.forward * 3 * Input.GetAxis("Vertical");
-        transform.position += transform.right * 3 * Input.GetAxis("Horizontal");
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+            {
+                EditTerrain.SetBlock(hit, new BlockAir());
+            }
+        }
     }
 }
