c98e9fe65d51150b32f2502272846760b7b9971a
Trio556 <alemat04@gmail.com>
2017-07-29 20:16:24 -0400

    Allowed for a more 'machine gun' like action when removing or adding blocks

--- Assets/Modify.cs
+++ Assets/Modify.cs
@@ -1,35 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Modify : MonoBehaviour
 {
+    bool isFire1Down = false;
+    bool isFire2Down = false;
+    double ticksBetweenFire = 150;
+    DateTime lastFire1Time;
+    DateTime lastFire2Time;
+    DateTime currentTimeFire;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        lastFire1Time = DateTime.Now;
+        lastFire2Time = DateTime.Now;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        currentTimeFire = DateTime.Now;
+        isFire1Down = IsButtonStillDown("Fire1", isFire1Down);
+        isFire2Down = IsButtonStillDown("Fire2", isFire2Down);
+
+        if (isFire1Down && (currentTimeFire - lastFire1Time).TotalMilliseconds >= ticksBetweenFire)
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
             {
                 EditTerrain.SetBlock(hit, new BlockAir());
             }
+
+            lastFire1Time = DateTime.Now;
         }
-        else if (CrossPlatformInputManager.GetButtonDown("Fire2"))
+        else if (isFire2Down && (currentTimeFire - lastFire2Time).TotalMilliseconds >= ticksBetweenFire)
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
             {
                 EditTerrain.SetBlock(hit, new BlockGrass(), true);
             }
+
+            lastFire2Time = DateTime.Now;
         }
+    }
+
+    private bool IsButtonStillDown(string actionName, bool currentValue)
+    {
+        if (CrossPlatformInputManager.GetButtonDown(actionName))
+            return true;
+        else if (CrossPlatformInputManager.GetButtonUp(actionName))
+            return false;
+
+        return currentValue;
     }
 }
